911c878a72d0bec5bc0c3912cfaa1eaedb5da8ba
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-14 18:50:30 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,8 +12,12 @@ class CashRegister
   end
   
   def apply_discount
-    @total -= @discount * 0.01 * @total
-    "After the discount, the total comes to $#{@total.to_i}."
+    if @discount == 0
+      "There is no discount to apply."
+    else
+      @total -= @discount * 0.01 * @total
+      "After the discount, the total comes to $#{@total.to_i}."
+    end
   end
   
 end
